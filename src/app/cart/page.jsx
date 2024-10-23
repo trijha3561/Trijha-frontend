9afc04fcff995ac31db23b0f08dd7430f2666c2b
Harshit <onmailharshit@gmail.com>
2024-10-23 11:54:41 +0530

    Added railway to cart

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -21,7 +21,7 @@ const CartPage = () => {
       }
 
       try {
-        const response = await fetch(`https://trijha-backend.vercel.app/cart?idToken=${token}`, {
+        const response = await fetch(`https://trijha-backend-production.up.railway.app/cart?idToken=${token}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
